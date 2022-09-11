55d1c0a4252aa867fffd8ae55f54d1d30787b08d
iyorozuya <iyaksha@tutanota.com>
2022-09-11 17:41:04 +0530

    chore: replaced koa with good old express

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,29 +1,13 @@
 import { greet } from '@ts-monorepo/core';
-import Koa from 'koa';
-const app = new Koa();
+import express from 'express';
+const app = express()
 
-// logger
+const PORT = 4000
 
-app.use(async (ctx, next) => {
-    await next();
-    const rt = ctx.response.get('X-Response-Time');
-    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
+app.get('/', (req, res) => {
+    res.send(greet('Katsura'))
+})
 
-// x-response-time
-
-app.use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    ctx.set('X-Response-Time', `${ms}ms`);
-});
-
-// response
-
-app.use((ctx) => {
-    ctx.body = greet('Zura Janai Katsura da');
-});
-
-app.listen(4000);
-console.log('Server running on port 4000');
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
